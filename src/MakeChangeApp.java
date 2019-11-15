d00f8b65ae19d37323184d030fe03520eb78e2d1
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-15 16:50:06 -0700

    Turning change into single digits

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -3,6 +3,21 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		System.out.println("Hello u");
+		double cashTendered = 15.65;
+//		System.out.println(cashTendered);
+		int [] cash = new int[4];
+		int amountDue = (int)(cashTendered*100);
+			for(int j =0;j<cash.length;j++) {
+				cash[j] = (int)(amountDue%10);
+				amountDue = amountDue/10;
+				System.out.println(cash[j]);
+				
+			}
+//			System.out.println("cash"+ cashTendered);
+		
+//		System.out.println(cashTendered);
+//		System.out.println(cash[1]);
+
 	}
 
 }
