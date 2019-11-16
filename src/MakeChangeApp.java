34b9fd20f6917819db05ee3ab3a5e8b0baeb4ec2
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-15 18:25:23 -0700

    Working on change

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -2,7 +2,7 @@
 public class MakeChangeApp {
 
 	public static void main(String[] args) {
-		double cashTendered = 1096.65;
+		double cashTendered = 1096.99;
 		int thousands = 0;
 		int hundreds = 0;
 		int fifties = 0;
@@ -12,69 +12,95 @@ public class MakeChangeApp {
 		int ones = 0;
 		int quarters = 0;
 		int dimes = 0;
+		int nickles = 0;
+		int pennies = 0;
+	
 //		System.out.println(cashTendered);
-		int [] cash = new int[6];
-		int amountDue = (int)(cashTendered*100);
-			for(int j =0;j<cash.length;j++) {
-				cash[j] = (int)(amountDue%10);
-				amountDue = amountDue/10;
-//				System.out.println(cash[j]);
-				
-			}
+		int[] cash = new int[6];
+	
+		int amountDue = (int) (cashTendered * 100);
+		for (int j = 0; j < cash.length; j++) {
+			cash[j] = (int) (amountDue % 10);
+			amountDue = amountDue / 10;
+		}
+		String changeConvert = cash[1] + "" + cash[0];
+		int change = Integer.parseInt(changeConvert);
 //			System.out.println(cash[3]);
-			while(cash[5] %10 !=0){
-				thousands += 1;
-				cash[5] -= 1;
-				
-			}
-			while( cash[4] %10 !=0) {
-				hundreds += 1;
-				cash[4] -= 1;
-				
-			}
-			while(cash[3] % 10!=0 && cash[3] >= 5 ) {
-				fifties +=1;
-				cash[3] -= 5;
-				
-			}
-			while(cash[3] % 10!=0 && cash[3] >= 2 ) {
-				twenties +=1;
-				cash[3] -= 2;
-				
-			}
-			while(cash[3] % 10 != 0) {
-				tens += 1;
-				cash[3] -= 1;
-			}
-			while(cash[2] >= 5 ) {
-				fives += 1;
-				cash[2] -= 5;
-				
-			}
-			while(cash[2] > 0) {
-				ones += 1;
-				cash[2] -= 1;
-			}
-			while(cash[1] >= 2 && cash[0] >=5 ) {
-				quarters += 1;
-				cash[1] -= 2;
-				cash[2] -= 5;
-				
-			}
-			while(cash[1] > 0) {
-				dimes += 1;
-				cash[1]-=1;
-			}
-			
-			System.out.println(thousands);
-			System.out.println(hundreds);
-			System.out.println(fifties);
-			System.out.println(twenties);
-			System.out.println(tens);
-			System.out.println(fives);
-			System.out.println(ones);
-			System.out.println(quarters);
-			System.out.println(dimes);
-	}
+		while (cash[5] % 10 != 0) {
+			thousands += 1;
+			cash[5] -= 1;
 
+		}
+		while (cash[4] % 10 != 0) {
+			hundreds += 1;
+			cash[4] -= 1;
+
+		}
+		while (cash[3] % 10 != 0 && cash[3] >= 5) {
+			fifties += 1;
+			cash[3] -= 5;
+
+		}
+		while (cash[3] % 10 != 0 && cash[3] >= 2) {
+			twenties += 1;
+			cash[3] -= 2;
+
+		}
+		while (cash[3] % 10 != 0) {
+			tens += 1;
+			cash[3] -= 1;
+		}
+		while (cash[2] >= 5) {
+			fives += 1;
+			cash[2] -= 5;
+
+		}
+		while (cash[2] > 0) {
+			ones += 1;
+			cash[2] -= 1;
+		}
+		System.out.println(change);
+		while (true) {
+			if(change > 25) {
+				quarters += 1;
+				change -= 25;
+				System.out.println(change);
+				continue;
+				}
+			
+			break;
+		}
+			
+			
+		
+		
+		
+//		System.out.println("1: " +cash[1]);
+//		while (cash[1] > 0 ) {
+//			dimes += 1;
+//			cash[1] -= 1;
+//		}
+//		System.out.println("0: " +cash[0]);
+//		while (cash[0] >= 5) {
+//			nickles += 1;
+//			cash[0] -= 1;
+//		}
+//		while (cash[0] > 0) {
+//			pennies += 1;
+//			cash[0] -= 1;
+//		}
+
+		System.out.println("thousand dollar bills: " + thousands);
+		System.out.println("hundred dollar bills: " + hundreds);
+		System.out.println("fifties: " + fifties);
+		System.out.println("twenties: " + twenties);
+		System.out.println("tens: " + tens);
+		System.out.println("fives: " + fives);
+		System.out.println("ones: " + ones);
+		System.out.println("quarters: " + quarters);
+		System.out.println("dimes: " + dimes);
+		System.out.println("nickles: " + nickles);
+		System.out.println("pennies: " + pennies);
+	
+	}
 }
