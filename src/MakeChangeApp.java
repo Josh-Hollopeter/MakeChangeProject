7f14950e03b202239007e8983912a2e650b7fdd1
Josh-Hollopeter <joshuahollopeter@gmail.com>
2019-11-17 21:17:24 -0700

    Closing scanner

--- src/MakeChangeApp.java
+++ src/MakeChangeApp.java
@@ -6,6 +6,7 @@ public class MakeChangeApp {
 
 	public static void main(String[] args) {
 		getChange();
+		input.close();
 
 	}
 
--- src/MakeChangeAppAlternate.java
+++ src/MakeChangeAppAlternate.java
@@ -6,6 +6,7 @@ public class MakeChangeAppAlternate {
 
 	public static void main(String[] args) {
 		getChange();
+		input.close();
 
 	}
 
@@ -19,18 +20,18 @@ public class MakeChangeAppAlternate {
 		double dimes = 0;
 		double nickles = 0;
 		double pennies = 0;
-		
+
 		cashTendered += 0.001;
-		
+
 		twenties = cashTendered / 20;
 		cashTendered %= 20;
 		tens = cashTendered / 10;
 		cashTendered %= 10;
-		fives = cashTendered /5;
+		fives = cashTendered / 5;
 		cashTendered %= 5;
-		ones = cashTendered /1;
+		ones = cashTendered / 1;
 		cashTendered %= 1;
-		
+
 		quarters = cashTendered / .25;
 		cashTendered %= .25;
 		dimes = cashTendered / .10;
@@ -38,25 +39,23 @@ public class MakeChangeAppAlternate {
 		nickles = cashTendered / .05;
 		cashTendered %= .05;
 		pennies = cashTendered / .01;
-	
 
-	
-		if ((int)twenties > 0)
-			System.out.println("Twenties: " + (int)twenties);
-		if ((int)tens > 0)
-			System.out.println("Tens: " + (int)tens);
-		if ((int)fives > 0)
-			System.out.println("Fives: " + (int)fives);
-		if ((int)ones > 0)
-			System.out.println("Ones: " + (int)ones);
-		if ((int)quarters > 0)
-			System.out.println("Quarters: " + (int)quarters);
-		if ((int)dimes > 0)
-			System.out.println("Dimes: " +(int) dimes);
-		if ((int)nickles > 0)
-			System.out.println("Nickles: " + (int)nickles);
-		if ((int)pennies > 0)
-			System.out.println("Pennies: " +(int) pennies);
+		if ((int) twenties > 0)
+			System.out.println("Twenties: " + (int) twenties);
+		if ((int) tens > 0)
+			System.out.println("Tens: " + (int) tens);
+		if ((int) fives > 0)
+			System.out.println("Fives: " + (int) fives);
+		if ((int) ones > 0)
+			System.out.println("Ones: " + (int) ones);
+		if ((int) quarters > 0)
+			System.out.println("Quarters: " + (int) quarters);
+		if ((int) dimes > 0)
+			System.out.println("Dimes: " + (int) dimes);
+		if ((int) nickles > 0)
+			System.out.println("Nickles: " + (int) nickles);
+		if ((int) pennies > 0)
+			System.out.println("Pennies: " + (int) pennies);
 
 	}
 
